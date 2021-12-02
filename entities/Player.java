b8f2fb3e33c955f68ba55064cd9b70c9ab99f3e8
Paridax <tgavdaly@gmail.com>
2021-12-01 21:32:40 -0500

    Initial commit

---
+++ entities/Player.java
@@ -0,0 +1,11 @@
+package entities;
+
+import utils.Entity;
+
+public class Player extends Entity 
+{
+    public void Main(String name)
+    {
+        System.out.println("Created new class with name " + name);
+    }
+}
