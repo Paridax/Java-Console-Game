2f6efcb705c70e3a9a3913078aa1486769eb4f2d
Paridax <tgavdaly@gmail.com>
2021-12-01 22:08:44 -0500

    Updated and tested new classes

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import entities.Player;
+import entities.Zombie;
+import utils.Utilities;
 
 class Main {
     public static Boolean gameLoop;
 
     public static void main(String[] args) {
+        Utilities.clear();
         Player player = new Player();
+        Zombie enemy = new Zombie();
         player.updateHealth(-5, false);
+
         System.out.println(player.health);
     }
 }
